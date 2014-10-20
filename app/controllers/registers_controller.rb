6f8c9dcd540f904914ceca118146c7122796f8ca
Alec Ferguson <alec.ferguson@gmail.com>
2014-10-20 00:00:14 -0700

    Removed commented code.

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -20,32 +20,6 @@ class RegistersController < ApplicationController
     end
   end
 
-  ## TODO: Move to UsersController
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @user.update(user_params)
-  #       format.html { redirect_to @user, notice: 'User was successfully updated.' }
-  #       format.json { head :no_content }
-  #     else
-  #       format.html { render action: 'edit' }
-  #       format.json { render json: @user.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  ## TODO: Move to UsersController
-  # DELETE /users/1
-  # DELETE /users/1.json
-  # def destroy
-  #   @user.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to users_url }
-  #     format.json { head :no_content }
-  #   end
-  # end
-
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def reg_params
