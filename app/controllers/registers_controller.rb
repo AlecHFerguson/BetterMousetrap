c6d9367e81aa0b1df48687184249925ef71406d8
Alec Ferguson <alec.ferguson@gmail.com>
2014-11-13 22:53:11 -0800

    Fixed a styling issue. Added link to register. Added parallel_test gem -- though it slowed down test execution!

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -15,7 +15,7 @@ class RegistersController < ApplicationController
         sign_in @user
         format.html { redirect_to gadgets_url, notice: 'User was successfully created.' }
       else
-        format.html { render action: 'new' }
+        format.html { render action: :index }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
