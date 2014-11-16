ef828d1a29fb4c190ca4c466181e83b376523237
Etienne Maugain <tiennoum@gmail.com>
2014-11-16 02:04:07 -0800

    Had to start from fresh

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141006052528) do
+ActiveRecord::Schema.define(version: 20141006050611) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
